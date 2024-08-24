9425c5cd4d654574a7c992154889d6e1422bd7fc
Your Name <sidratulpunno>
2024-08-24 23:06:10 +0600

    Added login page

---
+++ lib/widgets/text_field_input.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+class TextFieldInput extends StatelessWidget {
+  const TextFieldInput({
+    super.key,
+    required this.textInputType,
+    required this.textEditingController,
+    required this.hintText,
+    this.isPass = false,
+  });
+
+  final TextEditingController textEditingController;
+  final bool isPass;
+  final String hintText;
+  final TextInputType textInputType;
+
+  @override
+  Widget build(BuildContext context) {
+    final inputBorder =
+        OutlineInputBorder(borderSide: Divider.createBorderSide(context));
+    return TextField(
+      controller: TextEditingController(),
+      decoration: InputDecoration(
+          hintText: hintText,
+          border: inputBorder,
+          focusedBorder: inputBorder,
+          filled: true,
+          contentPadding: const EdgeInsets.all(8)),
+      keyboardType: textInputType,
+      obscureText: isPass,
+    );
+  }
+}
