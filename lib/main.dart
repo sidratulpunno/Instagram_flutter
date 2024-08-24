77ea1e8c969074bf036e4ad2b7839a4dfca5a61b
Your Name <sidratulpunno>
2024-08-24 11:53:49 +0600

    Added firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:instagram_flutter/responsive/mobile_screen_layout.dart';
 import 'package:instagram_flutter/responsive/responsive_layout_screen.dart';
@@ -5,7 +7,13 @@ import 'package:instagram_flutter/responsive/web_screen_layout.dart';
 
 import 'package:instagram_flutter/utils/colors.dart';
 
-void main() {
+import 'firebase_options.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
